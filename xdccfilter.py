c03b918be5152f174ef76f80f6bb458c0b812c9a
NIBL <admin@pantsu4u.com>
2015-08-16 11:55:05 +0100

    #NIBL XDCC Filter script for HexChat (v1.1)

--- xdccfilter.py
+++ xdccfilter.py
@@ -1,6 +1,6 @@
 # By Flintlock for NIBL
 __module_name__ = "NIBL XDCC Filter"
-__module_version__ = "1.0"
+__module_version__ = "1.1"
 __module_description__ = "Displays #NIBL XDCC announcements in a separate tab."
 
 import hexchat
@@ -8,29 +8,20 @@ import hexchat
 newTab = "(NIBL-XDCC)"
 channel = "#nibl"
 
-def GetNick(hostString):
-    # :nick!user@h.o.s.t
-    return hostString[1:].split("!")[0]
-
 def MakeTab():
     hexchat.command("QUERY {}".format(newTab))
 
 def Redirect(word, word_eol, userdata):
-    if word[2].lower() == channel:
-        nick = GetNick(word[0])
-        for user in hexchat.get_list("users"):
-            if nick == user.nick:
-                if user.prefix == "%":
-                    tabContext = hexchat.find_context(channel=newTab)
-                    if tabContext is None:
-                        MakeTab()
-                        tabContext = hexchat.find_context(channel=newTab)
-                    tabContext.emit_print("Channel Message", "{}".format(nick), "{}".format(word_eol[3][1:]), "%")
-                    return hexchat.EAT_ALL
-                else:
-                    break
-    return hexchat.EAT_NONE
+    if hexchat.get_info("channel").lower() == channel and len(word) > 2 and word[2] == "%":
+        tabContext = hexchat.find_context(channel=newTab)
+        if tabContext is None:
+            MakeTab()
+            tabContext = hexchat.find_context(channel=newTab)
+        tabContext.emit_print("Channel Message", word[0], word[1], word[2])
+        return hexchat.EAT_ALL
+    else:
+        return hexchat.EAT_NONE
 
-hexchat.hook_server("PRIVMSG", Redirect)
+hexchat.hook_print("Channel Message", Redirect)
 
 MakeTab()
